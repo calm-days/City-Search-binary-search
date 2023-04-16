bff7bb0ceb4f6c724d0a05661420bffbaf9815e1
Roman Liukevich <r.liukevich@gmail.com>
2023-04-17 01:34:32 +0200

    added binary search algorithm to CitiesController
    added binary search algorithm to CitiesController

--- city-search-ios-calm-days/city-search-ios-calm-days/CitiesController.swift
+++ city-search-ios-calm-days/city-search-ios-calm-days/CitiesController.swift
@@ -50,11 +50,92 @@ class CitiesController {
         // Attempting to decode the JSON file
         do {
             cityList = try JSONDecoder().decode([City].self, from: data)
+            asignSearchKeys()
+            sortCities()
             searchResultsIndex = (first: 0, last: cityList.count - 1)
         }
         catch {
             print("Could not load JSON file.")
         }
     }
- 
+    
+    // Method to assign search keys nameAndCountry to each city object
+    private func asignSearchKeys() {
+        for index in 0..<cityList.count {
+            cityList[index].searchValue = cityList[index].formattedString.lowercased()
+        }
+    }
+    
+    // Method to sort city objects based on the searchValue property
+    private func sortCities() {
+        cityList = cityList.sorted {
+            $0.searchValue < $1.searchValue
+        }
+        
+        reversedCityList = cityList.reversed()
+    }
+    
+    // Method to filter the city list based on the provided search query
+    func filter(searchQuery: String) {
+        let startDate = Date()
+        
+        // If the search query is empty, all items are visible.
+        if searchQuery.isEmpty {
+            searchResultsIndex = (first: 0, last: cityList.count - 1)
+        }
+        else {
+            updateSearchRange(search: searchQuery)
+        }
+        
+        //Printing speed of filtering
+        print("seconds: \(Date().timeIntervalSince(startDate)) - filtering prefix: \(searchQuery)")
+    }
+    
+    // Method to update the search results range based on the provided search query
+    private func updateSearchRange(search: String) {
+        let indexFirst = findFirstIndex(searchString: search)
+        let indexLast = findLastIndex(searchString: search)
+        
+        searchResultsIndex = (first: indexFirst, last: indexLast)
+    }
+    
+    // MARK: - Performing binary search
+    
+    private func findFirstIndex(searchString: String) -> Int {
+        var lowerBound = 0
+        var upperBound = cityList.count
+        let searchValue = searchString.lowercased()
+
+        while lowerBound < upperBound {
+            let midIndex = lowerBound + (upperBound - lowerBound) / 2
+            let currentItem = cityList[midIndex].searchValue
+
+            if searchValue <= currentItem {
+                upperBound = midIndex
+            } else {
+                lowerBound = midIndex + 1
+            }
+        }
+
+        return upperBound
+    }
+    
+    private func findLastIndex(searchString: String) -> Int {
+        var lowerBound = 0
+        var upperBound = cityList.count
+        let searchValue = searchString.lowercased()
+        
+        while lowerBound < upperBound {
+            let midIndex = lowerBound + (upperBound - lowerBound) / 2
+            let currentItem = reversedCityList[midIndex].searchValue
+            
+            if currentItem.starts(with: searchValue) || searchValue > currentItem {
+                upperBound = midIndex
+            } else {
+                lowerBound = midIndex + 1
+            }
+        }
+        
+        return cityList.count - upperBound
+    }
 }
