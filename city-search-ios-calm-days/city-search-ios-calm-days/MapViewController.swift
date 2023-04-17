66df05d294e4a82ae268a9d3afa48a29ff118991
Roman Liukevich <r.liukevich@gmail.com>
2023-04-17 19:12:17 +0200

    updated MapView controller with new functionality showing a pin with location
    updated MapView controller with new functionality showing a pin with location

--- city-search-ios-calm-days/city-search-ios-calm-days/MapViewController.swift
+++ city-search-ios-calm-days/city-search-ios-calm-days/MapViewController.swift
@@ -5,18 +5,54 @@ import MapKit
 class MapViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+    var annotation: MKPointAnnotation?
     
     var city: City? {
         didSet {
-            title = city!.formattedString
+            configureTitle()
         }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+        configureMapView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    private func configureMapView() {
         guard let city = city else { return }
-
+        
         mapView.region = MKCoordinateRegion(center: city.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+        mapView.layer.cornerRadius = 50.0
+        
+        annotation = MKPointAnnotation()
+        annotation?.title = city.formattedString
+        annotation?.coordinate = city.coordinate
+        mapView.delegate = self
+    }
+    
+    private func configureTitle() {
+        guard let city = city else { return }
+        
+        let titleLabel = UILabel()
+        titleLabel.text = city.formattedString
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        titleLabel.textColor = UIColor.label
+        titleLabel.sizeToFit()
+        navigationItem.titleView = titleLabel
+    }
+    
+}
+
+extension MapViewController {
+    func mapViewDidFinishRenderingMap(_ mapView: MKMapView, fullyRendered: Bool) {
+        if let annotation = annotation {
+            mapView.addAnnotation(annotation)
+            mapView.selectAnnotation(annotation, animated: true)
+        }
     }
 }
